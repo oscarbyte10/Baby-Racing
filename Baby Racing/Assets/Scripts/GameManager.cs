7e110ccb2f92254de210eaa8ea32a91b488d27c3
Óscar <oscarblanq@gmail.com>
2019-03-12 22:54:24 +0100

    Estructura de ficheros lista

---
+++ Baby Racing/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private GameObject ob1;
+    private GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ob1 = GameObject.FindGameObjectWithTag("Acelerador");
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
