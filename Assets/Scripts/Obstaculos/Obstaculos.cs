275b299eebe355ec8fb3e1102c2bdc8c7b6ac274
Mawcoo <matconol@epsg.upv.es>
2019-04-02 06:29:25 +0200

    Añadido obstaculo acelerador, no funcional.

---
+++ Assets/Scripts/Obstaculos/Obstaculos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculos : MonoBehaviour
+{
+
+    private ManuCar car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        car = GetComponent<ManuCar>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Hacer función que le devuelva un valor a la clase ConducirCoche
+    // dicho valor será el impulso que tomara en cierto tiempo, igual que el nitro
+
+}
