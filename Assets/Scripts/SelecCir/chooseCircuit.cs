56eedb389b06b395e35acf9d1e4b742c59d22cb9
Mawcoo <matconol@epsg.upv.es>
2019-04-14 16:57:27 +0200

    Diseño botones añadido.

--- Assets/Scripts/SelecCir/chooseCircuit.cs
+++ Assets/Scripts/SelecCir/chooseCircuit.cs
@@ -7,10 +7,16 @@ public class chooseCircuit : MonoBehaviour
 {
 
     private Button[] arrayButton;
+    public Sprite[] sprites;
+    private Image image;
+    private int count = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
         arrayButton = GetComponentsInChildren<Button>();
+        image = GameObject.Find("showCircuit").GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -27,4 +33,46 @@ public class chooseCircuit : MonoBehaviour
             Debug.Log(arrayButton[i].gameObject.name);
         }
     }
+
+    // Visualizamos el circuito anterior
+    public void beforeCircuit()
+    {
+        Debug.Log("Arriba");
+        count--;
+
+        if (count < 0)
+        {
+            // No haces nada
+            count++;
+        }
+        else
+        {
+            Debug.Log("Sprite: " + sprites[count].name);
+            image.sprite = sprites[count];
+        }
+    }
+
+    // Visualizamos el siguiente circuito
+    public void nextCircuit()
+    {
+        Debug.Log("Abajo");
+
+        count++;
+
+        if(count >= sprites.Length)
+        {
+            // No haces nada
+            count--;
+        }else
+        {
+            Debug.Log("Sprite: "+sprites[count].name);
+            image.sprite = sprites[count];
+            
+        }
+
+        
+       
+    }
+
+
 }
