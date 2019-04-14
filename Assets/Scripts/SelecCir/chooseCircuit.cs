6541710ba17c97f7c3dadf65ab741113b29a06f0
Mawcoo <matconol@epsg.upv.es>
2019-04-14 13:04:39 +0200

    Botones en selecion del circuito añadidos.

---
+++ Assets/Scripts/SelecCir/chooseCircuit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class chooseCircuit : MonoBehaviour
+{
+
+    private Button[] arrayButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        arrayButton = GetComponentsInChildren<Button>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void showButtonsInConsole()
+    {
+        for (int i = 0; i < arrayButton.Length; i++)
+        {
+            Debug.Log(arrayButton[i].gameObject.name);
+        }
+    }
+}
