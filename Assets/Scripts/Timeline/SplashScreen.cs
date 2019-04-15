f070ef182a624d235fca9ea7d4e9dff32cf4d5c8
Óscar <oscarblanq@gmail.com>
2019-04-15 19:39:19 +0200

    Animación terminada

---
+++ Assets/Scripts/Timeline/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class SplashScreen : MonoBehaviour
+{/*
+    private bool start = true;
+
+    public PlayableDirector director;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (start)
+        {
+            start = false;
+            director.Play();
+        }
+        
+    }*/
+}
