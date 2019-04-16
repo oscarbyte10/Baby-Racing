8b5447d184b4b5a1ebd090b08cae4efe79757bdf
Mawcoo <matconol@epsg.upv.es>
2019-04-16 13:54:15 +0200

    Director añadido

--- Assets/Scripts/Circuitos/CircuitoA.cs
+++ Assets/Scripts/Circuitos/CircuitoA.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Playables;
+
 
 public class CircuitoA : MonoBehaviour
 {
@@ -11,6 +13,9 @@ public class CircuitoA : MonoBehaviour
     public GameObject[] prefabsCoches;
     private GameObject prefabCoche;
     private Informacion info;
+    public PlayableDirector director;
+    private GameObject Canvas;
+    private int contador = 0;
     // Aquí metemos el clon del prefab de coche para en caso de ser necesario retocar sus componentes
     private GameObject player;
 
@@ -19,6 +24,12 @@ public class CircuitoA : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Canvas = GameObject.Find("HUD");
+        if (contador == 0)
+        {
+            empiezaAnimacion();
+        }
+
         info = FindObjectOfType<Informacion>();
         Debug.Log("Informacion de la escena del PJ: " + info.getNameCar());
 
@@ -40,8 +51,8 @@ public class CircuitoA : MonoBehaviour
         Instantiate(prefabCoche, new Vector3(-24.14f, 2, 8.5f), transform.rotation).SetActive(true);
         player = GameObject.Find(info.getNameCar()+"(Clone)");
 
-        cam.Follow = player.transform;
-        cam.LookAt = player.transform;
+       // cam.Follow = player.transform;
+        //cam.LookAt = player.transform;
 
         //player.GetComponent<Component>();
         //Debug.Log("Este es el número del escenario: " + info.getNumCar());
@@ -59,8 +70,19 @@ public class CircuitoA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        cam.m_Priority = 1000;
         
+            cam.enabled = true;
+        if (contador != 0)Canvas.SetActive(true);
+
     }
 
+    void empiezaAnimacion()
+    {
+      
+        director.Play();
+       
+        contador++;
 
+    }
 }
---
+++ Assets/Scripts/controlCamara.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+public class controlCamara : MonoBehaviour
+{/*
+    public CinemachineVirtualCamera camara;
+    public PlayableDirector director;
+    private int contador =0;
+    private GameObject Canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(contador == 0)
+        {
+            empiezaAnimacion();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        camara.m_Priority = 1000;
+        if(director.enabled == false)
+        {
+            camara.enabled = true;
+            Canvas = GameObject.Find("HUD");
+            Canvas.SetActive(true);
+        }
+        
+
+
+    }
+    void empiezaAnimacion()
+    {
+        Canvas = GameObject.Find("HUD");
+        director.Play();
+        Canvas.SetActive(false);
+        contador++;
+        
+    }*/
+}
